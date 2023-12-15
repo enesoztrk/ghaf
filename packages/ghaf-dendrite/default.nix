783b72a48d8e1f32eca82fa63b08f21cb2f42efa
Enes Öztürk <enes.ozturk@unikie.com>
2023-12-15 23:22:58 +0200

    hello

--- packages/ghaf-dendrite/default.nix
+++ packages/ghaf-dendrite/default.nix
@@ -7,13 +7,14 @@ buildGo119Module rec {
     pname = "ghaf-dendrite";
     version = "0.9.1";
 
-     src = final.pkgs.fetchFromGitHub {
+     src = pkgs.fetchFromGitHub {
         owner = "matrix-org";
         repo = "dendrite";
         rev = "v0.9.1";
         sha256 = "sha256-Fg7yfP5cM/mNAsIZAI/WGNLuz8l3vxyY8bb1NjuZELc=";
       };
             subPackages = ["cmd/dendrite-demo-pinecone"];
+          patches = [./turnserver-crendentials-flags.patch];
 
     vendorHash = "sha256-+9mjg8avOHPQTzBnfgim10Lfgpsu8nTQf1qYB0SLFys=";
 }
