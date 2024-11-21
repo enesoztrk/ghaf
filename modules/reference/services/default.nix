3966c64096442e8f233b44998860ce0e865a13e5
Enes Öztürk <enes.ozturk@unikie.com>
2024-11-21 19:19:46 +0200

    smcroute service nixification
    Signed-off-by: Enes Öztürk <enes.ozturk@unikie.com>

--- modules/reference/services/default.nix
+++ modules/reference/services/default.nix
@@ -5,12 +5,19 @@ let
   inherit (lib) mkEnableOption mkIf mkForce;
   cfg = config.ghaf.reference.services;
   isNetVM = "net-vm" == config.system.name;
+  isBusinessVM = "business-vm" == config.system.name;
+  isNetGroupCreated= (isNetVM);
+  isNetUserCreated= (isNetVM);
+  netUserName = "net-admin";
+  netSysGroupName = "net";
+
 in
 {
   imports = [
     ./dendrite-pinecone/dendrite-pinecone.nix
     ./dendrite-pinecone/dendrite-config.nix
     ./proxy-server/3proxy-config.nix
+    ./smcroute/smcroute.nix
     ./ollama/ollama.nix
   ];
   options.ghaf.reference.services = {
@@ -20,9 +27,25 @@ in
     ollama = mkEnableOption "ollama service";
   };
   config = mkIf cfg.enable {
+
+    # Conditional group creation
+    users.groups.${netSysGroupName} = mkIf isNetGroupCreated {
+    };
+
+    # Conditional user creation
+    users.users.${netUserName} = mkIf isNetVM {
+      isSystemUser = true;
+      description = "System user for managing network operations";
+      group = "${netSysGroupName}"; # Assign to the dynamically created group
+    };
+    
+    services.smcroute = mkIf (isNetVM){
+        user = mkForce "${netUserName}";
+    };
     ghaf.reference.services = {
       dendrite-pinecone.enable = mkForce (cfg.dendrite && isNetVM);
       proxy-server.enable = mkForce (cfg.proxy-business && isNetVM);
+
     };
   };
 }
--- modules/reference/services/dendrite-pinecone/dendrite-pinecone.nix
+++ modules/reference/services/dendrite-pinecone/dendrite-pinecone.nix
@@ -8,6 +8,7 @@
 }:
 let
   cfg = config.ghaf.reference.services.dendrite-pinecone;
+  inherit (config.ghaf.reference) services;
   dendrite-pineconePkg = pkgs.callPackage ../../../../packages/dendrite-pinecone/default.nix { };
   inherit (lib)
     mkEnableOption
@@ -60,67 +61,16 @@ in
       }
     ];
 
-    services.smcroute.enable= true;
-   /*  # ip forwarding functionality is needed for iptables
-    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-
-    # https://github.com/troglobit/smcroute?tab=readme-ov-file#linux-requirements
-    boot.kernelPatches = [
-      {
-        name = "multicast-routing-config";
-        patch = null;
-        extraStructuredConfig = with lib.kernel; {
-          IP_MULTICAST = yes;
-          IP_MROUTE = yes;
-          IP_PIMSM_V1 = yes;
-          IP_PIMSM_V2 = yes;
-          IP_MROUTE_MULTIPLE_TABLES = yes; # For multiple routing tables
-        };
-      }
-    ];
-    environment.systemPackages = [ pkgs.smcroute ];
-    systemd.services."smcroute" = {
-      description = "Static Multicast Routing daemon";
-      bindsTo = [ "sys-subsystem-net-devices-${cfg.externalNic}.device" ];
-      after = [ "sys-subsystem-net-devices-${cfg.externalNic}.device" ];
-      preStart = ''
-              configContent=$(cat <<EOF
+    services.smcroute = {
+      enable = lib.mkForce true;
+      bindingNic = "${cfg.externalNic}";
+      rules = ''
         mgroup from ${cfg.externalNic} group ${dendrite-pineconePkg.McastUdpIp}
         mgroup from ${cfg.internalNic} group ${dendrite-pineconePkg.McastUdpIp}
         mroute from ${cfg.externalNic} group ${dendrite-pineconePkg.McastUdpIp} to ${cfg.internalNic}
         mroute from ${cfg.internalNic} group ${dendrite-pineconePkg.McastUdpIp} to ${cfg.externalNic}
-        EOF
-        )
-        filePath="/etc/smcroute.conf"
-        touch $filePath
-          chmod 200 $filePath
-          echo "$configContent" > $filePath
-          chmod 400 $filePath
-
-        # wait until ${cfg.externalNic} has an ip
-        while [ -z "$ip" ]; do
-         ip=$(${pkgs.nettools}/bin/ifconfig ${cfg.externalNic} | ${pkgs.gawk}/bin/awk '/inet / {print $2}')
-              [ -z "$ip" ] && ${pkgs.coreutils}/bin/sleep 1
-        done
-        exit 0
       '';
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.smcroute}/sbin/smcrouted -n -s -f /etc/smcroute.conf";
-        #TODO sudo setcap cap_net_admin=ep ${pkgs.smcroute}/sbin/smcroute
-        # TODO: Add proper AmbientCapabilities= or CapabilityBoundingSet=,
-        #       preferably former and then change user to something else than
-        #       root.
-        User = "root";
-        # Automatically restart service when it exits.
-        Restart = "always";
-        # Wait a second before restarting.
-        RestartSec = "5s";
-      };
-      wantedBy = [ "multi-user.target" ];
     };
- */
     networking = {
       firewall.enable = true;
       firewall.extraCommands = "
